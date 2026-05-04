8463dfa547808bbda6b7364240024b5e414830d4
SANGEETHA K <sangeetha.kannan510_bci28@mepcoeng.ac.in>
2026-05-04 21:05:44 +0530

    Create gin.py

---
+++ gin.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUTS = input()
+
+string = input().strip()
+lowercase = [i for i in string if i in [i for i in 'abcdefghijklmnopqrstuvwxyz']]
+uppercase = [i for i in string if i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+odds = [i for i in string if i in '13579']
+evens = [i for i in string if i in '02468']
+
+letters = sorted(lowercase) + sorted(uppercase)
+numbers = sorted(odds) + sorted(evens)
+
+final_list = letters+numbers
+
+print(''.join(final_list))
