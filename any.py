a79d180191f1660699dbd230653b527b6f2ffead
SANGEETHA K <sangeetha.kannan510_bci28@mepcoeng.ac.in>
2026-05-04 20:53:13 +0530

    Create any.py

---
+++ any.py
@@ -0,0 +1,3 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N, numbers = int(input()), input().split()
+print(all(int(x) > 0 for x in numbers) and any(x == x[::-1] for x in numbers))
